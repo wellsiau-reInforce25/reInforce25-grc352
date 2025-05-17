411cef02be4774f4ad94672eb97baa61424f29b6
Workshop Participant <participant@workshops.aws>
2025-05-17 01:54:41 +0000

    Add boilerplate outputs and variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,6 @@
 ###### PLACEHOLDER FOR TERRAFORM CONFIG ######
-
+resource "aws_ecr_repository" "my_repository" {
+  name                 = "test-repo-tf"
+  image_tag_mutability = "IMMUTABLE"
+}
 ##############################################
